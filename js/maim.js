d780e3ffef8485e5958d65ff445d92ae0b4582e0
okapeeokapee <okapee420@gmail.com>
2020-08-27 21:30:50 +0900

    complete

--- js/maim.js
+++ js/maim.js
@@ -1,6 +1,7 @@
 'use strict'
 {
   const menuItems=document.querySelectorAll('.menu li a');
+  const contents=document.querySelectorAll('.content');
 
   menuItems.forEach(clickedItem=>{
     clickedItem.addEventListener('click',e=>{
@@ -14,6 +15,11 @@
         item.classList.remove('active');
       });
       clickedItem.classList.add('active');
+
+      contents.forEach(content=>{
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
     });
   });
 }
