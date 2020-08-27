875a787e3beea26effea06b306f4d8cd2eb282ca
okapeeokapee <okapee420@gmail.com>
2020-08-27 18:39:53 +0900

    add preventDefault

---
+++ js/maim.js
@@ -0,0 +1,15 @@
+'use strict'
+{
+  const menuItems=document.querySelectorAll('.menu li a');
+
+  menuItems.forEach(clickedItem=>{
+    clickedItem.addEventListener('click',e=>{
+      e.preventDefault();
+
+      menuItems.forEach(item=>{
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+    });
+  });
+}
